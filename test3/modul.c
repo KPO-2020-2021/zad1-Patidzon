e7d712aaeab1508a86973be0e1b64dc517df33d6
Patryk Dzon <259269@student.pwr.edu.pl>
2021-03-04 18:16:48 +0100

    change1

--- test3/modul.c
+++ test3/modul.c
@@ -3,5 +3,7 @@
  *  Zestaw wazniejszych stalych
  */
 
-const double PI = 3.141593;
-const double E  = 2.718282;
+/*nalezało dodać extern przed const*/
+
+extern const double PI = 3.141593;
+extern const double E  = 2.718282;
